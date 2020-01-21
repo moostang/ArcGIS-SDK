863ad59f7966911232483a2eb6f359f511e95925
Gyanendra Gurung <moostang@gmail.com>
2020-01-20 17:23:50 -0700

    Create GetPolylineFeaturelayersOnly.cs

---
+++ GetPolylineFeaturelayersOnly.cs
@@ -0,0 +1,21 @@
+        private async void GetActiveMapLayersAsync()
+        {
+            // Check if Active MapView exists
+            if (MapView.Active == null)
+            {
+                return;
+            }
+
+            _activeLayers.Clear();
+            
+            // Get Feature 
+            IEnumerable<Layer> layers = MapView.Active.Map.GetLayersAsFlattenedList().OfType<FeatureLayer>().Where(layer => layer.ShapeType.Equals(esriGeometryType.esriGeometryPolyline));
+
+            await QueuedTask.Run(() =>
+            {
+                foreach(FeatureLayer fLayer in layers)
+                {
+                    MessageBox.Show(fLayer.Name.ToString() + "is a polyline");                    
+                }
+            });
+        }
