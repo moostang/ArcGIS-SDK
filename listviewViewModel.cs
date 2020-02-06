1df6b38d8d1534ed4659de3502590b5e92824e06
Gyanendra Gurung <moostang@gmail.com>
2020-02-06 16:32:36 -0700

    Create listviewViewModel.cs

---
+++ listviewViewModel.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+
+namespace listview
+{
+    internal class Dockpane1ViewModel : DockPane
+    {
+        private const string _dockPaneID = "listview_Dockpane1";
+        public ObservableCollection<Document> Code { get; set; }
+        protected Dockpane1ViewModel()
+        {
+            
+            Code = new ObservableCollection<Document>();
+            Code.Add(new Document() { Description = "1", Notes = "One" });
+            Code.Add(new Document() { Description = "2", Notes ="two" });          
+            
+        }
+
+        internal class Document
+        {
+            public string Description { get; set; }
+            public string Notes { get; set; }           
+
+        }
+
+        /// <summary>
+        /// Show the DockPane.
+        /// </summary>
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            if (pane == null)
+                return;
+
+            pane.Activate();
+        }
+
+        /// <summary>
+        /// Text shown near the top of the DockPane.
+        /// </summary>
+        private string _heading = "My DockPane";
+        public string Heading
+        {
+            get { return _heading; }
+            set
+            {
+                SetProperty(ref _heading, value, () => Heading);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class Dockpane1_ShowButton : Button
+    {
+        protected override void OnClick()
+        {
+            Dockpane1ViewModel.Show();
+        }
+    }
+}
